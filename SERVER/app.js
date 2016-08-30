3b40a891e1f3a29e6105bc7c336e6bb9e1113acf
Oriol Torrillas <otorrillas@gmail.com>
2016-08-30 14:58:52 +0530

    App initial setup

---
+++ SERVER/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+
+
+app.use(cors());
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
+
+
+var tradeRouter = require('./routes/trader');
+var pmRouter = require('./routes/pm');
+
+
+app.use('/api/trader', traderRouter);
+app.use('/api/pm', pmRouter);
