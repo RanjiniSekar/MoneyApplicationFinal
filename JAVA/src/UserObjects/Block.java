d28bafb13c0f999d461e548bbb69420441631d76
adit77799 <040found@gmail.com>
2016-08-31 15:09:09 +0530

    trader ,order and block updated

--- JAVA/src/UserObjects/Block.java
+++ JAVA/src/UserObjects/Block.java
@@ -1,5 +1,53 @@
 package UserObjects;
 
 public class Block {
-
+	long blockId;
+	String symbol;
+	int quantity;
+	int orderType;
+	double stopPrice;
+	double limitPrice;
+	int status;
+	public long getBlockId() {
+		return blockId;
+	}
+	public void setBlockId(long blockId) {
+		this.blockId = blockId;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getOrderType() {
+		return orderType;
+	}
+	public void setOrderType(int orderType) {
+		this.orderType = orderType;
+	}
+	public double getStopPrice() {
+		return stopPrice;
+	}
+	public void setStopPrice(double stopPrice) {
+		this.stopPrice = stopPrice;
+	}
+	public double getLimitPrice() {
+		return limitPrice;
+	}
+	public void setLimitPrice(double limitPrice) {
+		this.limitPrice = limitPrice;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
 }
--- JAVA/src/UserObjects/Trader.java
+++ JAVA/src/UserObjects/Trader.java
@@ -1,11 +1,61 @@
 package UserObjects;
 
-public class Trader extends User{
+import java.util.ArrayList;
 
-	@Override
-	void testFunction() {
-		// TODO Auto-generated method stub
-		
+public class Trader extends User{
+	
+	ArrayList<Order> Order_list;
+	ArrayList<Block> Block_list_generated ;
+	ArrayList<Block> Block_list_sent ;
+
+	
+	
+	
+
+	public ArrayList<Order> getOrder_list() {
+		return Order_list;
 	}
+
+
+
+
+
+	public void setOrder_list(ArrayList<Order> order_list) {
+		Order_list = order_list;
+	}
+
+
+
+
+
+	public ArrayList<Block> getBlock_list_generated() {
+		return Block_list_generated;
+	}
+
+
+
+
+
+	public void setBlock_list_generated(ArrayList<Block> block_list_generated) {
+		Block_list_generated = block_list_generated;
+	}
+
+
+
+
+
+	public ArrayList<Block> getBlock_list_sent() {
+		return Block_list_sent;
+	}
+
+
+
+
+
+	public void setBlock_list_sent(ArrayList<Block> block_list_sent) {
+		Block_list_sent = block_list_sent;
+	}
+
+
 
 }
