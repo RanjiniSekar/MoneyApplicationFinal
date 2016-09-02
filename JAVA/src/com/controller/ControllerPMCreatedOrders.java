aafea60a6f715c0a6bb50ecfbd0c79c89203ed0f
Ceren Savasan <cerensavasan@gmail.com>
2016-09-02 17:11:09 +0530

    Sample Unirest query

--- JAVA/src/com/controller/ControllerPMCreatedOrders.java
+++ JAVA/src/com/controller/ControllerPMCreatedOrders.java
@@ -7,6 +7,10 @@ package com.controller;
 
 import TestModules.JTableDataPopulation.JsonParsing;
 import UserObjects.Order;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,15 +19,19 @@ import UserObjects.Order;
 public class ControllerPMCreatedOrders {
 
     public static void handleOrder(Order receivedOrder) {
-        //HERE YOU NEED TO HANDLE GETTING PM ID FROM SESSION
-        
-        
-        //HERE YOU NEED TO HANDLE GETTING THE ID OF THE TRADER WHOS NAME WE HAVE STORED IN STRING
-        
-        
-        //THEN YOU PARSE THE ORDER OBJECT COMPLETELY. RIGHT NOW WE ONLY PARSE THE STRING OF TRADER 
-        //NAME AND LIST OF SINGLE ORDERS
-        System.out.println(JsonParsing.parseJsonFromObject(receivedOrder));
+            //HERE YOU NEED TO HANDLE GETTING PM ID FROM SESSION
+            String receivedJson = JsonParsing.parseJsonFromObject(receivedOrder);
+            System.out.println(receivedJson);
+            
+            //HERE YOU NEED TO HANDLE GETTING THE ID OF THE TRADER WHOS NAME WE HAVE STORED IN STRING
+            
+            
+            //THEN YOU PARSE THE ORDER OBJECT COMPLETELY. RIGHT NOW WE ONLY PARSE THE STRING OF TRADER
+            //NAME AND LIST OF SINGLE ORDERS
+            
+            //IS THIS HOW THIS WORKS?
+            Unirest.post("http://httpbin.org/post")
+                    .body(receivedJson);
     }
 }
     
