75a8dfaa240db56158ac33eaa29b85784d1b837e
Govind-jha <govind@123>
2016-09-02 10:33:27 +0530

    added geters and setters for the class

--- JAVA/src/UserObjects/SingleOrder.java
+++ JAVA/src/UserObjects/SingleOrder.java
@@ -3,21 +3,22 @@ package UserObjects;
 public class SingleOrder {
 	
     
-	long SingleOrderId;
-	long portfolioId ;
-	long OrderId;
-	long BlockId;
-	String symbol;
-	int quantity;
-	int action;
-	double stopPrice;
-	double limitPrice;
-	double pricePaid;
-	int stockExchange;
-	int accountType;
-	int orderType;
-	int status;
-        long pmId;
+	private long SingleOrderId;
+	private long portfolioId ;
+	private long OrderId;
+	private long BlockId;
+	private String symbol;
+	private int quantity;
+	private int action;
+	private double stopPrice;
+	private double limitPrice;
+	private double pricePaid;
+	private int stockExchange;
+	private int accountType;
+	private int orderType;
+	private int status;
+        private long pmId;
+        private long assignedTo;
 
     public SingleOrder(long SingleOrderId, long portfolioId, long OrderId, long BlockId, String symbol, int quantitiy, int action, double stopPrice, double limitPrice, double pricePaid, int stockExchange, int accountType, int orderType, int status) {
         this.SingleOrderId = SingleOrderId;
@@ -154,6 +155,133 @@ public class SingleOrder {
     public String toString() {
         return "SingleOrder{" + "SingleOrderId=" + SingleOrderId + ", portfolioId=" + portfolioId + ", OrderId=" + OrderId + ", BlockId=" + BlockId + ", symbol=" + symbol + ", quantitiy=" + quantity + ", action=" + action + ", stopPrice=" + stopPrice + ", limitPrice=" + limitPrice + ", pricePaid=" + pricePaid + ", stockExchange=" + stockExchange + ", accountType=" + accountType + ", orderType=" + orderType + ", status=" + status + '}';
     } 
-    
+
+    public long getSingleOrderId() {
+        return SingleOrderId;
+    }
+
+    public void setSingleOrderId(long SingleOrderId) {
+        this.SingleOrderId = SingleOrderId;
+    }
+
+    public long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public void setPortfolioId(long portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+
+    public long getOrderId() {
+        return OrderId;
+    }
+
+    public void setOrderId(long OrderId) {
+        this.OrderId = OrderId;
+    }
+
+    public long getBlockId() {
+        return BlockId;
+    }
+
+    public void setBlockId(long BlockId) {
+        this.BlockId = BlockId;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getAction() {
+        return action;
+    }
+
+    public void setAction(int action) {
+        this.action = action;
+    }
+
+    public double getStopPrice() {
+        return stopPrice;
+    }
+
+    public void setStopPrice(double stopPrice) {
+        this.stopPrice = stopPrice;
+    }
+
+    public double getLimitPrice() {
+        return limitPrice;
+    }
+
+    public void setLimitPrice(double limitPrice) {
+        this.limitPrice = limitPrice;
+    }
+
+    public double getPricePaid() {
+        return pricePaid;
+    }
+
+    public void setPricePaid(double pricePaid) {
+        this.pricePaid = pricePaid;
+    }
+
+    public int getStockExchange() {
+        return stockExchange;
+    }
+
+    public void setStockExchange(int stockExchange) {
+        this.stockExchange = stockExchange;
+    }
+
+    public int getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(int accountType) {
+        this.accountType = accountType;
+    }
+
+    public int getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(int orderType) {
+        this.orderType = orderType;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public long getPmId() {
+        return pmId;
+    }
+
+    public void setPmId(long pmId) {
+        this.pmId = pmId;
+    }
+
+    public long getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(long assignedTo) {
+        this.assignedTo = assignedTo;
+    }
     
 }
