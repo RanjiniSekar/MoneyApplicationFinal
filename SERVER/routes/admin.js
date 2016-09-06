1ba2b120d379a7a9480366e0a21118063381d6f8
Oriol Torrillas <otorrillas@gmail.com>
2016-09-06 10:47:26 +0530

    username specific queries

--- SERVER/routes/admin.js
+++ SERVER/routes/admin.js
@@ -62,5 +62,36 @@ router.route('/')
     });
 
 
+router.route('/:username')
+    .get(function (req, res) {
+        db.query('SELECT * from user WHERE username = ?',
+            req.params.username,
+            function (error, results, fields) {
+                // error will be an Error if one occurred during the query 
+                // results will contain the results of the query 
+                // fields will contain information about the returned results fields (if any)
+                if (error) {
+                    console.log('Error performing the query');
+                    res.status(500).send(error);
+                } else {
+                    res.json(fields);
+                }
+            });
+    })
+    .put(function (req, res) {
+        db.query('UPDATE user SET password = ? WHERE username = ?', [req.params.username, req.body.password],
+            function (error, results, fields) {
+                // error will be an Error if one occurred during the query 
+                // results will contain the results of the query 
+                // fields will contain information about the returned results fields (if any)
+                if (error) {
+                    console.log('Error performing the query');
+                    res.status(500).send(error);
+                } else {
+                    res.json(fields);
+                }
+            });
+    });
+
 
 module.exports = router;
