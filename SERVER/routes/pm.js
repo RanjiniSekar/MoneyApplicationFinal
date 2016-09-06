6aff18b1c66f3de9d36a0d1d8e315ea858b08d1d
Oriol Torrillas <otorrillas@gmail.com>
2016-09-06 12:12:22 +0530

    Added test interface to test auto-assignment

--- SERVER/routes/pm.js
+++ SERVER/routes/pm.js
@@ -114,7 +114,6 @@ router.route('/orders')
                     } else {
                         console.log(results);
                         req.body.assignedTo = assignedTo;
-
                         insert_order(req);
                     }
                 });
@@ -124,5 +123,27 @@ router.route('/orders')
 
     });
 
+
+router.route('/test')
+    .get(function (req, res) {
+        var assignedTo = 25;
+
+        db.query({
+                sql: 'SELECT assigned_to FROM pm_order WHERE assigned_to = (SELECT order_id, COUNT(order_id) FROM single_order GROUP BY order_id ORDER BY COUNT(order_id) ASC LIMIT 1);'
+            }, [req.body.portfolioManagerId, req.body.assignedTo],
+            function (error, results, fields) {
+                // error will be an Error if one occurred during the query 
+                // results will contain the results of the query 
+                // fields will contain information about the returned results fields (if any)
+                if (error) {
+                    console.log('Error performing the query:');
+                    console.log(error);
+                    res.status(500).send(error);
+                } else {
+                    console.log(results);
+                    res.json(results);
+                }
+            })
+    });
 
 module.exports = router;
