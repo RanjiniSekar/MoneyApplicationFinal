fcbf42b366444ae466eac137b4ed7d6889111b23
otorrillas <otorrillas@sapient.com>
2016-09-06 22:36:47 -0400

    Mailing integration

--- SERVER/mailer/test.js
+++ SERVER/mailer/test.js
@@ -5,12 +5,25 @@ nodemailer.sendmail = true;
 var options = {
 	host: 'localhost',
 	port: 25,
-	auth: {
-	    user: 'nodeserver',
-	    pass: 'S@pientTeam4'
-	}
+	tls:{
+        rejectUnauthorized: false
+    }
 }
 
 var transporter = nodemailer.createTransport(options);
 
+var mailOptions = {
+	from: 'info@moneytree.com',
+        to: 'otorrillas@gmail.com',
+        subject: 'Govind is a pussy',
+        text: 'Hey there I am using WhatsApp'
+
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
 
