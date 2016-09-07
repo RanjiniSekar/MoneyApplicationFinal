bd6c85164c19cb68df26fe038e7aea8eba632f03
Oriol Torrillas <otorrillas@gmail.com>
2016-09-07 20:19:34 +0530

    Added success and error view for broker

--- SERVER/routes/views.js
+++ SERVER/routes/views.js
@@ -7,4 +7,15 @@ router.route('/trade')
         res.sendFile(path.join(__dirname + '/../html/home_broker.html'));
     });
 
+router.route('/success')
+    .get(function (req, res) {
+        res.sendFile(path.join(__dirname + '/../html/success.html'));
+    });
+
+router.route('/error')
+    .get(function (req, res) {
+        res.sendFile(path.join(__dirname + '/../html/error.html'));
+    });
+
+
 module.exports = router;
