767f0dcafb5c543b5e6fe3d14696a8080644fcd1
Oriol Torrillas <otorrillas@gmail.com>
2016-09-07 22:06:08 +0530

    broker updateTrade improvement

--- SERVER/routes/broker.js
+++ SERVER/routes/broker.js
@@ -10,8 +10,8 @@ router.route('/updateTrade')
         pool.getConnection(function (err, conn) {
             if (req.body.action == "accept") {
                 conn.query({
-                        sql: "UPDATE trader_block SET price_executed=? WHERE block_id= (select block_id from temp_link where uid = ?)"
-                    }, [req.body.price, req.body.uid],
+                        sql: "UPDATE trader_block SET price_executed = ?, status = ?  WHERE block_id= (select block_id from temp_link where uid = ?)"
+                    }, [req.body.price, "Executed", req.body.uid],
                     function (error, results, fields) {
                         // error will be an Error if one occurred during the query 
                         // results will contain the results of the query 
@@ -22,8 +22,8 @@ router.route('/updateTrade')
                         } else {
                             console.log("TRADER_BLOCK update successfully");
                             conn.query({
-                                    sql: "UPDATE single_order SET price_executed=? WHERE block_id= (select block_id from temp_link where uid = ?)"
-                                }, [req.body.price, req.body.uid],
+                                    sql: "UPDATE single_order SET price_executed = ?, status = ? WHERE block_id= (select block_id from temp_link where uid = ?)"
+                                }, [req.body.price, "Executed", req.body.uid],
                                 function (error, results, fields) {
                                     // error will be an Error if one occurred during the query 
                                     // results will contain the results of the query 
@@ -48,8 +48,54 @@ router.route('/updateTrade')
             } else if (req.body.action == "reject") {
                 // order has been rejected
                 conn.query({
+                        sql: "UPDATE trader_block SET status = ? WHERE block_id= (select block_id from temp_link where uid = ?)"
+                    }, ["Rejected", req.body.uid],
+                    function (error, results, fields) {
+                        // error will be an Error if one occurred during the query 
+                        // results will contain the results of the query 
+                        // fields will contain information about the returned results fields (if any)
+                        if (error) {
+                            console.log('Error performing the query');
+                            res.status(500).send(error);
+                        } else {
+                            console.log("TRADER_BLOCK update successfully");
+                            conn.query({
+                                    sql: "UPDATE single_order SET status = ? WHERE block_id= (select block_id from temp_link where uid = ?)"
+                                }, ["Rejected", req.body.uid],
+                                function (error, results, fields) {
+                                    // error will be an Error if one occurred during the query 
+                                    // results will contain the results of the query 
+                                    // fields will contain information about the returned results fields (if any)
+                                    if (error) {
+                                        console.log('Error performing the query:');
+                                        console.log(error);
+                                        res.status(500).send(error);
+                                    } else {
+                                        console.log("SINGLE_ORDER update successful");
 
-                }, [req.body.uid]);
+                                        conn.query({
+                                                sql: "DELETE FROM temp_link WHERE uid = ?"
+                                            }, [req.body.uid],
+                                            function (error, results, fields) {
+                                                if (error) {
+                                                    console.log('Error performing the query:');
+                                                    console.log(error);
+                                                    res.status(500).send(error);
+                                                } else {
+                                                    console.log('temp_link deleted successfully')
+                                                    res.status(204).send(results);
+                                                }
+                                            });
+
+                                    }
+                                });
+
+
+
+
+                        }
+                    }
+                );
             }
             conn.release();
         });
